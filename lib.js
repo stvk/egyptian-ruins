b20401acc2dc67b50f2e8612d8c3da09fe5540fe
stvk <github@stvk.org>
2018-12-21 00:51:53 +1100

    messing around with svg

---
+++ lib.js
@@ -0,0 +1,26 @@
+/*
+The JavaScript function below parses and returns the parameters.
+*/
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+/*
+Select a parameter, and set a default value to the variable if it doesn't exist
+
+Use like this:
+var mytext = getUrlParam('parameter_name','Default Value');
+*/
+function getUrlParam(parameter, defaultvalue){
+    var urlparameter = defaultvalue;
+    if(window.location.href.indexOf(parameter) > -1){
+        urlparameter = getUrlVars()[parameter];
+        }
+    return urlparameter;
+}
+
